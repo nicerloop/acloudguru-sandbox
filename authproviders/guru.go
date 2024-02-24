71b1155701d4939c1b008151e14510f18a7a4216
Judikael <judikael@gaki.eu>
2024-02-24 16:24:47 +0000

    feat: Add google auth provider

--- authproviders/guru.go
+++ authproviders/guru.go
@@ -8,6 +8,9 @@ import (
 	"github.com/go-rod/rod/lib/input"
 )
 
+// DOM selector which tell that the login process is done and guru page are display
+const selectorLogged = "button[data-cy='tab--cloud-sandboxes']"
+
 type AuthGuru struct{}
 
 func (a *AuthGuru) AuthId() string {
@@ -17,8 +20,15 @@ func (a *AuthGuru) AuthId() string {
 func (a *AuthGuru) Login(acgSandboxesUrl string, username string, password string) *rod.Page {
 	browser := rod.New().MustConnect().NoDefaultDevice()
 	page := browser.MustPage(acgSandboxesUrl)
+
+	if page.MustHas(selectorLogged) {
+		return page
+	}
+
 	page.MustElement("input[name='email']").MustInput(username).MustType(input.Enter)
 	page.MustElement("input[name='password']").MustInput(password).MustType(input.Enter)
+	page.MustWaitDOMStable()
+
 	if page.MustHas("input[name='captcha']") {
 		if defaults.Show {
 			page.MustElement("input[name='captcha']").MustFocus()
@@ -26,5 +36,11 @@ func (a *AuthGuru) Login(acgSandboxesUrl string, username string, password strin
 			log.Fatalf("Warning: CAPTCHA in login form, use -rod=show option")
 		}
 	}
+
+	if page.MustHas("div.auth0-global-message-error") {
+		errorMessage := page.MustElement("div.auth0-global-message-error").MustText()
+		log.Fatalf("Error: %s", errorMessage)
+	}
+
 	return page
 }
